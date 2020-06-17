c7274eafb8a7c394de3a2878b46b59268c8e633b
Zaher Abd Ulmoula <zaher88abd@gmail.com>
2020-06-17 12:54:17 -0300

    reformate the code

--- convert_IMDB_data_to_csv.py
+++ convert_IMDB_data_to_csv.py
@@ -1,58 +1,66 @@
 import os
+import sys
 from os.path import join as pjoin
 from bson.json_util import loads, dumps
 import pandas as pd
 from pandas import json_normalize
 import io
 
-main_folder="/home/zaher/DataFolder"
-meta_data="./groundtruth"
-poster_data="Movie_Poster_Dataset"
+main_folder = "/home/zaher/DataFolder"
+meta_data = "./groundtruth"
+poster_data = "Movie_Poster_Dataset"
+
 
 def readingJsonFile(file):
-    lines=""
+    lines = ""
     try:
-        with io.open(file, 'r',encoding='utf-16') as f:    
+        with io.open(file, 'r', encoding='utf-16') as f:
             for line in f.readlines():
-                
+
                 if line is None:
                     break
                 if not line.strip().startswith("\"_id\""):
-                    lines+=line+"\n"
+                    lines += line + "\n"
     except:
-        with io.open(file, 'r',encoding='utf-8') as f:    
+        with io.open(file, 'r', encoding='utf-8') as f:
             for line in f.readlines():
-                
+
                 if line is None:
                     break
                 if not line.strip().startswith("\"_id\""):
-                    lines+=line+"\n"
+                    lines += line + "\n"
 
-   
-    contents=lines.replace("}","},").rstrip()[:-1]
+    contents = lines.replace("}", "},").rstrip()[:-1]
 
-    contents="["+contents+"]"
+    contents = "[" + contents + "]"
     # print(contents)
     # contents=dumps(contents)
-    contents=loads(contents)
+    contents = loads(contents)
     # print(type(contents))
-    
-    df=pd.DataFrame(json_normalize(contents))
+
+    df = pd.DataFrame(json_normalize(contents))
     # print(type(df))
     return df
 
-def joinDFs(mainDF,df):
+
+def joinDFs(mainDF, df):
     if mainDF is None:
         return df
-    mainDF=pd.concat([mainDF,df])
+    mainDF = pd.concat([mainDF, df])
     return mainDF
 
-mainDF=None
-count=1
-for f in os.listdir(pjoin(meta_data)):
-    print(f,count)
-    mainDF=joinDFs(mainDF,readingJsonFile(pjoin(meta_data,f)))
-    # print(len(mainDF))
-    count+=1
 
-mainDF.to_csv("./test.csv",index=False)
+def main(arag):
+    mainDF = None
+    count = 1
+    for f in os.listdir(pjoin(meta_data)):
+        print(f, count)
+        mainDF = joinDFs(mainDF, readingJsonFile(pjoin(meta_data, f)))
+        # print(len(mainDF))
+        count += 1
+
+    mainDF.to_csv("./" + arag[0], index=False)
+
+
+if __name__ == '__main__':
+    main(arag=sys.argv)
